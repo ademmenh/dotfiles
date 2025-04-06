6b8d43b5cc0a35cde0b6a05f191d0af46654966e
ademmenh <ademmenh777@gmail.com>
2025-04-06 21:05:57 +0100

    Update nvim config, update lualine appearance

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,11 +5,60 @@ return {
     },
     config = function () 
       require('lualine').setup({
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-      }
-      })
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          section_separators = '', 
+          component_separators = '',
+          globalstatus = true,
+        },
+        sections = {
+            lualine_a = {
+              {
+                  'mode',
+                  icons_enabled = true,
+                  align = 'left',
+                  color = { fg = nil },
+                  icon = '  ',
+                  separator = nil,
+                  padding = 2,
+              }
+            },
+            lualine_b = {
+                {
+                    'branch',
+                    icon = {
+                      '',
+                      color = { fg = nil },
+                    },
+                    padding = 2,
+                }
+            },
+            lualine_c = {
+                {
+                  'buffers',
+                  show_filename_only = true,
+                  hide_filename_extension = false,
+                  show_modified_status = false,
+                  mode = 0,
+                  show_only_active = true,
+                  max_length = 1,
+                  filetype_names = {
+                      TelescopePrompt = 'Telescope',
+                      dashboard = 'Dashboard',
+                      packer = 'Packer',
+                      fzf = 'FZF',
+                      alpha = 'Alpha',
+                      ["neo-tree"] = 'Neo-tree',
+                  },
+                  use_mode_colors = false,
+                  padding = 2,
+                },
+            },
+            lualine_x = {'progress'},
+            lualine_y = {}
+        },
+    })
     end,
 }
 
