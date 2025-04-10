c4eb95509f1525d6b200c6d84d4761f94bbc7e03
ademmenh <ademmenh777@gmail.com>
2025-04-10 13:44:32 +0100

    Update nvim config, Add ident-blankline config, current line highliter

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,61 +4,61 @@ return {
         'nvim-tree/nvim-web-devicons'
     },
     config = function ()
-      require('lualine').setup({
-        options = {
-          icons_enabled = true,
-          theme = 'auto',
-          section_separators = '',
-          component_separators = '',
-          globalstatus = true,
-        },
-        sections = {
-            lualine_a = {
-              {
-                  'mode',
-                  icons_enabled = true,
-                  align = 'left',
-                  color = { fg = nil },
-                  icon = ' ',
-                  separator = nil,
-                  padding = 2,
-              }
+        require('lualine').setup({
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                section_separators = '',
+                component_separators = '',
+                globalstatus = true,
             },
-            lualine_b = {
-                {
-                    'branch',
-                    icon = {
-                      '',
-                      color = { fg = nil },
-                    },
-                    padding = 4,
-                }
-            },
-            lualine_c = {
-                {
-                  'buffers',
-                  show_filename_only = true,
-                  hide_filename_extension = false,
-                  show_modified_status = true,
-                  mode = 0,
-                  show_only_active = true,
-                  max_length = 1,
-                  filetype_names = {
-                      TelescopePrompt = 'Telescope',
-                      dashboard = 'Dashboard',
-                      packer = 'Packer',
-                      fzf = 'FZF',
-                      alpha = 'Alpha',
-                      ["neo-tree"] = 'Neo-tree',
-                  },
-                  use_mode_colors = false,
-                  padding = 2,
+            sections = {
+                lualine_a = {
+                    {
+                        'mode',
+                        icons_enabled = true,
+                        align = 'left',
+                        color = { fg = nil },
+                        icon = ' ',
+                        separator = nil,
+                        padding = 2,
+                    }
                 },
+                lualine_b = {
+                    {
+                        'branch',
+                        icon = {
+                            '',
+                            color = { fg = nil },
+                        },
+                        padding = 4,
+                    }
+                },
+                lualine_c = {
+                    {
+                        'buffers',
+                        show_filename_only = true,
+                        hide_filename_extension = false,
+                        show_modified_status = true,
+                        mode = 0,
+                        show_only_active = true,
+                        max_length = 1,
+                        filetype_names = {
+                            TelescopePrompt = 'Telescope',
+                            dashboard = 'Dashboard',
+                            packer = 'Packer',
+                            fzf = 'FZF',
+                            alpha = 'Alpha',
+                            ["neo-tree"] = 'Neo-tree',
+                        },
+                        use_mode_colors = false,
+                        padding = 2,
+                    },
+                },
+                lualine_x = {'progress'},
+                lualine_y = {}
             },
-            lualine_x = {'progress'},
-            lualine_y = {}
-        },
-    })
+        })
     end,
 }
 
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,12 +2,12 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = "TSUpdate",
     config = function ()
-      local treesitter = require("nvim-treesitter.configs")
-      treesitter.setup({
-        ensure_installed = {"c", "vim", "vimdoc", "query", "markdown", "lua", "javascript", "json", "yaml", "markdown"},
-        highlight = { enable = true },
-        ident = { enable = true },
-      })
+        local treesitter = require("nvim-treesitter.configs")
+        treesitter.setup({
+            ensure_installed = {"c", "vim", "vimdoc", "query", "markdown", "lua", "javascript", "json", "yaml", "markdown"},
+            highlight = { enable = true },
+            ident = { enable = true },
+        })
     end
 }
 
